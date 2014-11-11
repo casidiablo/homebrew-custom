5be47188585b73ca8811b8855fa9475eaf33acf2
Fedor Nikolaev <fsqcds@gmail.com>
2014-11-11 21:55:01 +0300

    Update to 0.3.5.

--- Casks/popcorn-time.rb
+++ Casks/popcorn-time.rb
@@ -1,6 +1,6 @@
 class PopcornTime < Cask
-  version '0.3.4'
-  sha256 '3c6a7ab2f33a6fa9df0bf3332227ddf3e068a864fe195b8fd3ffff508ea80d4b'
+  version '0.3.5'
+  sha256 '3726f4ca44a1323d0bdcbf428730ce50ddfca02cbbd42fab28b7055a51040b6e'
 
   url "https://cdn.popcorntime.io/build/Popcorn-Time-#{version}-Mac.dmg"
   homepage 'http://popcorntime.io/'
