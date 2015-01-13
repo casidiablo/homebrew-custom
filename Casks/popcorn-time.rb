5762585ff3de59bee298f55b92e2d645cf8811d2
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-01-13 19:28:14 +0100

    Change URL back to popcorntime.io

--- Casks/popcorn-time.rb
+++ Casks/popcorn-time.rb
@@ -2,7 +2,7 @@ cask :v1 => 'popcorn-time' do
   version '0.3.6'
   sha256 '48419eaa34ab31ca071e010ff49c065ba23ad06bd8f1b50349308980e937dfeb'
 
-  url "http://popcorn.obsidian.goender.net/build/Popcorn-Time-#{version}-Mac.dmg"
+  url "https://cdn.popcorntime.io/build/Popcorn-Time-0.3.6-Mac.dmg"
   homepage 'http://popcorntime.io/'
   license :gpl
 
