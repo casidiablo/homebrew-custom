bbebe262c03dd2c3d9ae37194578c89d02dea015
Erhan Abay <erhanabay@gmail.com>
2015-07-11 23:40:33 +0300

    change popcorn time url where it's redirected

--- Casks/popcorn-time.rb
+++ Casks/popcorn-time.rb
@@ -2,7 +2,7 @@ cask :v1 => 'popcorn-time' do
   version '0.3.8-0'
   sha256 'f1c7edbb24ee582b44e30eb880f81c9140d7c8035f6484283a1f0e45fb840ddc'
 
-  url "https://get.popcorntime.io/build/Popcorn-Time-#{version}-Mac.dmg", :user_agent => :fake
+  url "http://188.166.124.227/build/Popcorn-Time-#{version}-Mac.dmg", :user_agent => :fake
   appcast 'https://popcorntime.io/update.json',
     :sha256 => '3354d21ad453807bd663a20861dc3da36f3d2860929d30fc30ee15f87e8622af',
     :format => :unknown
